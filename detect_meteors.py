36ba71f8699bb86b143ba7f3c3b26e6414d98d3f
fjpadilladea <fran00pda@gmail.com>
2022-08-08 18:43:55 +0200

    Mejoras añadidas: soporte a imágenes y vídeos

--- detect_meteors.py
+++ detect_meteors.py
@@ -1,61 +1,97 @@
 import cv2 as cv
 import numpy as np
+import os
 
-def detect(video, mask_image):
+def img_processing(img, mask):
+    # Blur image (decrease sensibility)
+    blurred = cv.GaussianBlur(img,(17,17),0)
+    
+    # Find edges using Canny's algorithm
+    edges = cv.Canny(blurred,100,200)
+    
+    # Closing (dilation - erosion)
+    closed = cv.morphologyEx(edges, cv.MORPH_CLOSE, (7,7), iterations = 5)
+    
+    # Masking
+    masked = cv.bitwise_and(closed, mask)
+    
+    # Find lines using Hough Lines Transform
+    # The function returns None if there are no lines found
+    lines = cv.HoughLinesP(masked,1,np.pi/180,30,minLineLength=20,maxLineGap=10)
+    
+    # Lines are considered as possible meteors
+    # If a line is found, return True. Otherwise, return False
+    if lines is not None:
+        return True
+    else:
+        return False
 
-    vid = cv.VideoCapture(video)
-    
-    if (vid.isOpened() == False):
-        print('Error opening video ' + video)
-    
-    # Read the mask file
+def detect(file, mask_image):
+
+    if mask_image is None:
+    # If a mask image is not passed as an argument,
+    # use a white image as the mask
+        mask = np.ones((1,1,1),dtype=np.uint8)*255
+    else:
+     # Read the mask file
     # It is necessary to open it as a grayscale image in order to do de bitwise AND operation
     # between the mask and the 'closed' image, which has only one channel
-    mask = cv.imread(mask_image, cv.IMREAD_GRAYSCALE)
+        mask = cv.imread(mask_image, cv.IMREAD_GRAYSCALE)    
     
-    # Resize the mask
-    resizedMask = cv.resize(mask, (1920,1080), interpolation = cv.INTER_AREA)
-
-    while(vid.isOpened()):
+    # Check file extension
+    split_name = os.path.splitext(file)
+    extension = split_name[1]
     
-        # Get the next frame of the video
-        ret, frame = vid.read()
-    
-        # If there are more frames available
-        if ret == True:
-            
-            # Resize image
-            resized = cv.resize(frame, (1920,1080), interpolation = cv.INTER_AREA)
-            
-            # Blur image (decrease sensibility)
-            blurred = cv.GaussianBlur(resized,(17,17),0)
-            
-            # Find edges using Canny's algorithm
-            edges = cv.Canny(blurred,100,200)
-            
-            # Closing (dilation - erosion)
-            closed = cv.morphologyEx(edges, cv.MORPH_CLOSE, (7,7), iterations = 5)
-            
-            # Masking
-            masked = cv.bitwise_and(closed, resizedMask)
-            
-            # Find lines using Hough Lines Transform
-            # The function returns None if there are no lines found
-            lines = cv.HoughLinesP(masked,1,np.pi/180,30,minLineLength=20,maxLineGap=10)
-            
-            # If a meteor is detected, return True
-            if lines is not None:
-                vid.release()
-                return True
-            
-
-        # If there are no more frames in the video, break out of the loop
+    # If the file is an image
+    if extension == '.jpeg' or extension == '.jpg' or extension == '.png':
+        img = cv.imread(file)
+        
+        #Resize the mask
+        dimensions = img.shape
+        height = dimensions[0]
+        width = dimensions[1]
+        resizedMask = cv.resize(mask, (width,height), interpolation = cv.INTER_AREA)
+        
+        detection = img_processing(img, resizedMask)
+        # If a meteor is detected, return True. Otherwise, return False
+        if detection == True:
+            return True
         else:
-            break
+            return False 
+    
+    # If the file is a video
+    elif extension == '.mp4' or extension == '.avi':
+        vid = cv.VideoCapture(file)
+        
+        if (vid.isOpened() == False):
+            print('Error opening video ' + file)
 
-    # Release video capture
-    vid.release()
-    
-    # No meteors were detected: return False
-    return False
-    
+        while(vid.isOpened()):
+        
+            # Get the next frame of the video
+            ret, frame = vid.read()
+        
+            # If there are more frames available
+            if ret == True:
+                # Resize the mask
+                dimensions = frame.shape
+                height = dimensions[0]
+                width = dimensions[1]
+                resizedMask = cv.resize(mask, (width,height), interpolation = cv.INTER_AREA)
+                
+                detection = img_processing(frame)
+
+                # If a meteor is detected, close the video file and return True
+                if detection is True:
+                    vid.release()
+                    return True
+                
+            # If there are no more frames in the video, break out of the loop
+            else:
+                break
+
+        # Release video capture
+        vid.release()
+        
+        # No meteors were detected: return False
+        return False
