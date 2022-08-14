2b66af5e09c2ba3b80ee7aff09d9bedd01846323
fjpadilladea <fran00pda@gmail.com>
2022-08-14 13:35:13 +0200

    Modificado: código reescrito como una función

--- send_messages.py
+++ send_messages.py
@@ -1,19 +1,20 @@
 import requests
 
-with open('#FILE#, 'rb') as file:
-    my_file = {
-      'file': file
-    }
-    
-    headers = {
-      "Authorization":"Bearer #BOT TOKEN#"
-            }
+def upload(path):
+    with open(path, 'rb') as file: 
+        my_file = {
+        'file': file
+        }
+        
+        headers = {
+        "Authorization":"Bearer #BOT TOKEN#"
+        }
 
-    payload={ 
-      "filename": "#FILENAME#",
-      "channels": '#pruebas_tfg',
-    }
-    
-    r = requests.post("https://slack.com/api/files.upload", params=payload, files=my_file, headers=headers)
-    print(r.text)
+        payload = { 
+        "filename": #FILENAME#,
+        "channels": '#CHANNEL#',
+        }
+        
+        r = requests.post("https://slack.com/api/files.upload", params=payload, files=my_file, headers=headers)
+        print(r.text)
 
